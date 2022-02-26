71063104fa384f577a6c307520ae371c1fd5988d
WIlliam Silva <98836080+silvawillx@users.noreply.github.com>
2022-02-26 17:35:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'silvawillx',
+  youtube: 'channel/UCwYyHc9q3BumrXh2_iZIrsQ',
+  facebook: '100009520867048',
+  instagram: 'silvawillx',
+  twitter: 'willsilvax'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
